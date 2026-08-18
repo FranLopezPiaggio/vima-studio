8cde700cf4ab5f97e1a45feb3443f59ccf6b5d42
FranLP <francolopezpiaggio@gmail.com>
2026-08-18 20:32:17 -0300

    workflow and nextconfig github pages deploy

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true, // Requerido porque GitHub Pages no soporta la optimización de imágenes en servidor de Next.js
+  },
 };
 
 export default nextConfig;
